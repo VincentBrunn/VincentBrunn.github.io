1e72b7ab3bd0cf5790bb7f1757b13214a9f60bb6
Vincent Brunn <Brunnvincent@gmail.com>
2025-03-01 18:51:51 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -170,8 +170,8 @@ const CONFIG = {
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'medium', // medium | dev
-    username: '@Vincent_Brunn', // to hide blog section, keep it empty
-    limit: 1, // How many articles to display. Max is 10.
+    username: 'Vincent_Brunn', // to hide blog section, keep it empty
+    limit: 10, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
